f801e77d0f9d211bdfaf6fd0bb6a5664acd8387f
Mashrur Mahmud Hossain <mahmudmashrur0@gmail.com>
2026-01-29 15:32:09 +0600

    Creating a login form and shdow while clicked on login button

--- app.js
+++ app.js
@@ -3,6 +3,52 @@ const test=['web devloper', 'Cyber-Security expert', 'Back-end Devloper'];
 let textScript = document.querySelector('.animate-text');
 let menu = document.querySelector('.menu2');
 let bars = document.querySelector('.bar');
+const image = ['./img/engineer-removebg-preview.png', './img/engineer.jpg', './img/mash.png'];
+
+
+let countIndex = 0;
+
+
+const ListA = [{name:'Apple', prize:200, image1:'./img/engineer-removebg-preview.png'}];
+const ListB = [{name:'JackFruite', prize:300, image1:'./img/engineer.jpg'}, {name:'Apple', prize:200, image1:'./img/engineer-removebg-preview.png'}];
+
+// const handleMatch = ()=>{
+//     const match = document.getElementById('match');
+//     for(const A of ListA){
+//         for(const B of ListB){
+//             if(A?.name == B?.name){
+//                 const div = document.createElement('div');
+//                 div.innerHTML=`
+//                  <h1>${A?.name}</h1>
+//                  <img src='${A?.image1}' />
+
+//                 `;
+//                 let addhon = match.appendChild(div);
+//                 return addhon;
+//             }
+//         }
+//     }
+// }
+
+
+
+
+const handleSlide = ()=>{
+    countIndex += 1;
+    
+    const sliderImage = document.getElementById('sliderImage');
+    const newDiv = document.createElement('div');
+    newDiv.className='images';
+    newDiv.innerHTML=`
+      <img class='w-full' src='${image[countIndex]}'/>
+    `
+    sliderImage.appendChild(newDiv)
+  
+    
+    
+
+}
+
 
 
 
@@ -83,6 +129,11 @@ gsap.to('.text-animation', {
     stagger:0.2
     
 })
+
+
+
+
+
 
 
 
